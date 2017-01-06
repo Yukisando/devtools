5438321ef8515f88beb397d0fd841c2786965977
Nathan De Castro <ndeca001@gold.ac.uk>
2017-01-06 15:18:47 +0000

    better shell

--- shell-aliases.cmd
+++ shell-aliases.cmd
@@ -1,8 +1,17 @@
-doskey ~=cd %homepath%
-doskey ~d=cd %homepath%/Desktop
-doskey ~w=cd %homepath%/Documents/Work
-doskey ~s=cd %homepath%/OneDrive/Dev/Tools/Scripts
-doskey ~t=cd %homepath%/OneDrive/Dev/temp
-doskey ~m=cd %homepath%/Samples
+@echo off
+
+doskey p= mpv $*
+doskey ydl= youtube-dl $*
+doskey ~=  cd %homepath%
+doskey ~d= cd %homepath%/Desktop
+doskey ~w= cd %homepath%/Documents/Work
+doskey ~s= cd %homepath%/OneDrive/Dev/Tools/Scripts
+doskey ~dev= cd %homepath%/OneDrive/Dev
+doskey ~soft= cd %homepath%/OneDrive/Dev/Tools/Softwares
+doskey ~t= cd %homepath%/OneDrive/Dev/temp
+doskey ~m= cd %homepath%/Samples
+doskey open=start .
 doskey $=exit
+doskey nyan=mpv %homepath%/Samples/nyan.webm --no-border --fs
+
 cls
