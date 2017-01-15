93c4dbea21841a1b90f0f56084b019d35590558b
Nathan De Castro <ndeca001@gold.ac.uk>
2017-01-15 16:56:48 +0000

    Added support for consoleZ and some bin files

--- PathSetupCMD.bat
+++
@@ -1,1 +0,0 @@
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "IF x"%COMSPEC%"==x%CMDCMDLINE% (cd /D C:\Users\natha\OneDrive\Work)"
---
+++ bin/setmpv.bat
@@ -0,0 +1,38 @@
+@echo off
+cls
+
+:savePrompt
+echo Save settings to MPV from appdata?(y/n)
+set INPUT=
+set /P INPUT=Type input: %=%
+If /I "%INPUT%"=="y" goto save
+If /I "%INPUT%"=="n" goto noSave
+echo Incorrect input & goto savePrompt
+
+:save
+copy %appdata%\MPV\* %homepath%\OneDrive\Dev\Tools\Softwares\MPV\conf\
+echo Files save from appdata to MPV successfully.
+
+:noSave
+echo.
+echo Not saving...
+
+:loadPrompt
+echo Load settings from MPV onto appdata?(y/n)
+set INPUT=
+set /P INPUT=Type input: %=%
+If /I "%INPUT%"=="y" goto load
+If /I "%INPUT%"=="n" goto noLoad
+echo Incorrect input & goto loadPrompt
+
+:load
+copy %homepath%\OneDrive\Dev\Tools\Softwares\MPV\conf\* %appdata%\MPV
+echo Loaded files from MPV to appdata successfully.
+
+:noLoad
+echo.
+echo Not loading...
+
+echo.
+echo All done :)
+pause
